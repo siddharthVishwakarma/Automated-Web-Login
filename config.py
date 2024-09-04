94bac695080e66d820b0f9342b899389c3700258
Siddharth Vishwakarma <sid.sv14@gmail.com>
2024-09-04 20:07:02 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# Configuration details
+LOGIN_URL = 'https://example.com/login'
+USERNAME = 'your_username'
+PASSWORD = 'your_password'
+
+# Path to the WebDriver executable (e.g., ChromeDriver)
+WEBDRIVER_PATH = '/path/to/chromedriver'
